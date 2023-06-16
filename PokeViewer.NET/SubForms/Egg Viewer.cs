cf172228d2245a82cf0f3f0ce001953261874bc0
Lucas Mundim <lucas.mundim044@gmail.com>
2023-06-16 11:55:01 -0300

    update pointers

--- PokeViewer.NET/SubForms/Egg Viewer.cs
+++ PokeViewer.NET/SubForms/Egg Viewer.cs
@@ -48,8 +48,8 @@ namespace PokeViewer.NET.SubForms
         private int eggcount = 0;
         private int sandwichcount = 0;
         private int shinycount = 0;
-        private readonly uint EggData = 0x044AAE00;
-        private readonly uint PicnicMenu = 0x0453B020;
+        private readonly uint EggData = 0x044C12D8;
+        private readonly uint PicnicMenu = 0x04551020;
         private readonly byte[] BlankVal = { 0x01 };
         public IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x44CCAE8, 0x348, 0x10, 0xD8, 0x28 };
         private ulong OverworldOffset;
